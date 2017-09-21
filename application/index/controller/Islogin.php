52f056e68b8cbae54eed4896ac76c42a6f1d77fa
Dave <948009662@qq.com>
2017-09-21 22:11:59 +0800

    【Added】添加登陆验证

---
+++ application/index/controller/Islogin.php
@@ -0,0 +1,18 @@
+<?php
+	/**
+	* 
+	*/
+	namespace app\index\controller;
+	use \think\Controller;
+	use think\Session;
+
+	class Islogin extends Controller
+	{
+		protected $isLogin;
+		function __construct()
+		{
+			parent::__construct();
+			$this->isLogin = Session::get('user_name')?Session::get('user_name'):'';
+		}
+	}
+?>
