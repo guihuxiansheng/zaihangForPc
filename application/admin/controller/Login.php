ba8cd6977877c01eba0f18925381d99ff84da435
Dave <948009662@qq.com>
2017-09-25 16:25:41 +0800

    【Added】添加topic动态数据

--- application/admin/controller/Login.php
+++ application/admin/controller/Login.php
@@ -1,5 +1,6 @@
 <?php
 	namespace app\admin\controller;
+	use \think\Session;
 	/**
 	* 
 	*/
@@ -10,6 +11,31 @@
 	    {
 	    	
 	       return $this->fetch();
+	    }
+	    public function login()
+	    {
+	    	$user_name = input('username');
+	    	$user_pwd = md5(input('password'));
+	    	try{
+	    		$info = db('user')->where("user_pwd='$user_pwd' and user_phone='$user_name' and admin=1")->find();
+	    		if($info){
+					Session::set('manage_user',$info['user_name']);
+					return json_encode(Array(
+						'status'=>0,
+						'message'=> '登录成功！'
+					));
+				}else{
+					return json_encode(Array(
+						'status'=> 1,
+						'message'=> '登陆失败'
+					));
+				}
+	    	}catch(\Exception $e){
+	    		return json_encode(Array(
+					'status'=>1,
+					'message'=> '查询错误'
+				));
+	    	}
 	    }
 	}
 ?>
--- application/index/controller/Topics.php
+++ application/index/controller/Topics.php
@@ -5,7 +5,16 @@
 	{
 		public function index()
 		{
+			$cate_city = input('cate_city');
+
+			$cate = model('Topics')->getCate();
+			$cate_list = model('Topics')->getCateNext();
+			$topic = model('Topics')->getTopic();
+			$this->assign('cate',$cate);
+			$this->assign('cate_list',$cate_list);
+			$this->assign('topic',$topic);
 			return $this->fetch();
 		}
+
 	}
 ?>
