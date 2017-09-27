5e92e422b9a49113c81b792e3d146586e5b234aa
guihuxiansheng <948009662@qq.com>
2017-09-27 15:51:34 +0800

    Revert "mzx详情页后台、个人资料编辑和头像上传"

--- application/index/controller/Detail.php
+++ application/index/controller/Detail.php
@@ -5,21 +5,8 @@
 	{
 		public function index()
 		{
-	        $info = db("expert")->where("uid=11")->find();
-	        $this->assign("info",$info);			
-
-	        $topicinfo = db("topic")->where("uid=11")->select();
-	        $this->assign("topicinfo",$topicinfo);			
-	        
 			return $this->fetch();
 		}
-		public function meetsubmit()
-		{
-			db('meet')->insert(input());
-			return input();
-		}
-
-		
 		
 	}
 ?>
